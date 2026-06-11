6a6746b1b6caa69c44837267646d231d9063792a
Basile Mouret <145753507+Basile-Mouret@users.noreply.github.com>
2026-06-11 18:41:09 +0200

    corrected cfl for 2D advection

--- FiniteVolumes/src/equations/advection2D.jl
+++ FiniteVolumes/src/equations/advection2D.jl
@@ -6,7 +6,7 @@ end
 
 Advection2D(; c::NTuple{2, Float64}) = Advection2D(c)
 
-num_vars(::Advection2D) = 2
+num_vars(::Advection2D) = 1
 
 function flux(eq::Advection2D, UL::Vector{Float64}, UR::Vector{Float64}, normal::NTuple{2, Float64})
     cn = eq.c ⋅ normal
@@ -18,19 +18,17 @@ function flux(eq::Advection2D, UL::Vector{Float64}, UR::Vector{Float64}, normal:
 end
 
 function compute_dt(mesh::Mesh2D, eq::Advection2D, values::Matrix{Float64}, CFL::Float64)::Float64
-    min_dt = typemax(Float64)
-    for cell_idx in 1:length(mesh.cells)
-        sum_outgoing = 0.0
-        for face_idx in 1:length(mesh.faces)
-            if eq.c ⋅ mesh.face_normals[face_idx] > 0
-                sum_outgoing += (mesh.face_lengths[face_idx]  * (eq.c ⋅ mesh.face_normals[face_idx]))/mesh.cell_measure[cell_idx]
-            end
+    sum_outgoing = zeros(length(mesh.cells))
+    for (face_id, (CL, CR)) in enumerate(mesh.face_cells)
+        cn = eq.c ⋅ mesh.face_normals[face_id]
+        l  = mesh.face_lengths[face_id]
+        if cn > 0 && CL != 0
+            sum_outgoing[CL] += cn * l
+        elseif cn < 0 && CR != 0
+            sum_outgoing[CR] += (-cn) * l
         end
-        dt = CFL / sum_outgoing
-        min_dt = min(min_dt, dt)
     end
-        
-    return min_dt 
+    return minimum(CFL .* mesh.cell_measure ./ sum_outgoing)
 end
 
 
