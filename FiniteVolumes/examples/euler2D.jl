683bad462a26f65747557dc69ed9df0523290bd9
Basile Mouret <145753507+Basile-Mouret@users.noreply.github.com>
2026-06-16 09:47:45 +0200

    before optimizations

--- FiniteVolumes/examples/euler2D.jl
+++ FiniteVolumes/examples/euler2D.jl
@@ -1,35 +1,47 @@
 using FiniteVolumes
 
-mesh = load_mesh2D("meshes/triangle_conv3.msh")
+mesh = load_mesh2D("meshes/mesh_hole.msh")
+println(keys(mesh.boundary_tags))
 
-gamma = 1.4
-eq = Euler2D(gamma, :exact)
 
-x0, y0 = 0.5, 0.5
+γ = 1.4
+eq = Euler2D(γ, :exact)
+
+
+M = 3.3  # inflow Mach number
+ρ  = 1.0
+u  = M
+v  = 0.0
+p  = 1.0 / γ
+E  = p / (γ - 1) + 0.5 * ρ * (u^2 + v^2)
 
 function ic(x)
-    r2 = (x[1]-x0)^2 + (x[2]-y0)^2
     rho = 1.0
     u, v = 0.0, 0.0
-    p = r2 <= 0.3^2 ? 1.0 : 0.1   # high pressure circle without vacuum
-    E = p/(gamma-1.0) + 0.5*rho*(u^2+v^2)
+    p = 1.0/γ
+    E = p/(γ-1.0) + 0.5*rho*(u^2+v^2)
     return [rho, rho*u, rho*v, E]
 end
 
+W_inflow = [ρ, ρ*u, ρ*v, E]
+
+
 boundary_conditions = Dict(
-                           "boundary_1" => ReflectingEuler2D(),
-                           "boundary_2" => ReflectingEuler2D(),
-                           "boundary_3" => ReflectingEuler2D(),
-                           "boundary_4" => ReflectingEuler2D(),
+                           "Bottom"         => Outflow(),
+                           "Top"            => Outflow(),
+                           "Left"           => Dirichlet2D((x,t) -> W_inflow),
+                           "Right"          => Outflow(),
+                           "HoleBoundary"   => ReflectingEuler2D(),
                           )
 
-max_time_steps = 1000
-final_time = 5.0
+max_time_steps = 10000
+final_time = 3.0
 CFL = 0.9
 
 
 U_hist, dt_hist = solve(mesh, eq, boundary_conditions, ic; max_time_steps = max_time_steps, CFL = CFL, final_time=final_time)
 
-u_scalar = [mat[:, 2].^2 + mat[:, 3].^2 for mat in U_hist]
-animate_cell_values(mesh, u_scalar; dt_hist = dt_hist)
+ρ_hist = [mat[:, 1] for mat in U_hist]
+u_hist = [mat[:, 2].^2 + mat[:, 3].^2 for mat in U_hist]
+animate_cell_values(mesh, ρ_hist; dt_hist = dt_hist)
 
